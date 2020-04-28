862fb974fbc093e5a8696d2e426740c341b22361
Franco2219 <64434693+Franco2219@users.noreply.github.com>
2020-04-28 20:34:49 -0300

    Add files via upload

---
+++ consolelognumeros1clase.js
@@ -0,0 +1,14 @@
+MiEdad=22;
+TuEdad=20;
+NuestraEdad=42;
+
+console.log(MiEdad + TuEdad);
+
+function sumar(numero1, numero2, numero3, numero4, numero5) {
+	resultado = 0+1+2+3+4;
+
+	return resultado;
+	
+for (var i = 0; i < Things.length; i++) {
+	Things[i]
+}
